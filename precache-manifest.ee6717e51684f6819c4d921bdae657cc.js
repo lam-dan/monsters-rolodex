0e57d1f2abf303c1284d8534889770634362e0d9
Daniel <danlam@ucdavis.edu>
2019-07-25 21:18:48 -0700

    Updates

---
+++ precache-manifest.ee6717e51684f6819c4d921bdae657cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa71057e22940155992fdeed1ee30fa7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1812bd1aae04d3b86dfc",
+    "url": "/monsters-rolodex/static/css/main.5a1585ac.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1812bd1aae04d3b86dfc",
+    "url": "/monsters-rolodex/static/js/main.1edf6027.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
